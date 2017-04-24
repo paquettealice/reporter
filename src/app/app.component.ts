60274474e70dcd822b11d52c333dab302191e24b
paquettepy <paquette.py@gmail.com>
2017-04-23 22:31:46 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+
+import { Report } from './reports/report.model';
+import { ReportService, SelectedReportService } from './reports/report.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  reports: Observable<Array<Report>>;
+  selectedReport: Observable<Report>;
+
   title = 'app works!';
   tabs = ['Sales report', 'something else', 'another something else'];
+
+  constructor(private reportService: ReportService, private selectedReportService: SelectedReportService) {
+    this.reports = reportService.reports;
+    this.selectedReport = selectedReportService.selectedReport;
+  }
 }
+
